e966bb5c7d6a47c2571e8756c9770349f86a2249
gabegodoi <gabegodoi@gmail.com>
2024-08-06 09:20:54 -0700

    added functions to shopping_list_maker.py

--- the_shopping_list_maker.py
+++ the_shopping_list_maker.py
@@ -7,35 +7,39 @@ Task 1: Write a function that lets the user add items to a list.
 Task 2: Include a function to remove items from the list.
 
 Task 3: Add a function that prints out the entire list in a formatted way.
-
-Note: The goal of this is to be a program. The recommendation is to use a while loop that will allow 
-the user to continue to add, remove, and print off their shopping list until 
-they decide to "quit", also known as breaking the loop.
 '''
 
-def list_maker():
-    shopping_list = []
-    while True:
-        action = input("What would you like to do?\n[A]dd item to list\n[R]emove item from list\n[D]isplay list\n[Q]uit\n").lower()
-        if action == "a":
-            item = input("Item to add: ").lower()
-            shopping_list.append(item)
-            print(f"\n{item} added to list\n")
-        elif action == "r":
-            item = input("Item to remove: ").lower()
-            if item in shopping_list:
-                shopping_list.remove(item)
-                print(f"\n{item} removed from list\n")
-            else:
-                print(f"\n{item} not found in list\n")
-        elif action == 'd':
-            print("ITEMS:")
-            if len(shopping_list) == 0:
-                print("No items in list\n") 
-            else:
-                for item in shopping_list:
-                    print(f"- {item}")
-        elif action == "q":
-            break
+def add_to_list():
+    item = input("Item to add: ").lower()
+    shopping_list.append(item)
+    print(f"\n{item} added to list\n")
 
-list_maker()
+def remove_from_list():
+    item = input("Item to remove: ").lower()
+    if item in shopping_list:
+        shopping_list.remove(item)
+        print(f"\n{item} removed from list\n")
+    else:
+        print(f"\n{item} not found in list\n")
+
+def print_entire_list():
+    print("ITEMS:")
+    if len(shopping_list) == 0:
+        print("No items in list\n") 
+    else:
+        for item in shopping_list:
+            print(f"- {item}")
+
+shopping_list = []
+while True:
+    action = input("\nWhat would you like to do?\n[A]dd item to list\n[R]emove item from list\n[D]isplay list\n[Q]uit\n\n").lower()
+    if action == "a":
+        add_to_list()
+    elif action == "r":
+        remove_from_list()
+    elif action == 'd':
+        print_entire_list()
+    elif action == "q":
+        break
+    else:
+        print("\nAction not recognized.\n")
